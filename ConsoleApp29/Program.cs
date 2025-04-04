b2599b94f7bcaefe3ce1404ebaf4a5a245776c12
Amer Sheikh Alhara <abo.keroz443@hotmail.com>
2025-04-04 14:48:12 +0200

    Stad class.

--- ConsoleApp29/Program.cs
+++ ConsoleApp29/Program.cs
@@ -8,100 +8,21 @@ namespace ConsoleApp29
     {
         static void Main(string[] args)
         {
-            Console.CursorVisible = false;
-            //Console.SetWindowSize(sizeX, sizeY);
-            //Console.SetBufferSize(sizeX+1, sizeY+1);
- 
-            //Persons inom Y-axeln 27-50, X-axeln 1-99
-            //NEWS X-axel = 2 till 65, Y-axel = 17 till 24
-            //STATUS X-axel = 2 till 65, Y-axel = 7 till 14
-            //PRISON X-axel = 69 till 99, Y-axel = 7 till 24
-
-            
-            List<Polis> poliser = new List<Polis>();
-            for (int i = 0; i < 5; i++)
             {
-                poliser.Add(new Polis(new List<string>()));
-            }
-
-            List<Tjuv> tjuvar = new List<Tjuv>();
-            for (int i = 0; i < 5; i++)
-            {
-                tjuvar.Add(new Tjuv(new List<string>()));
-            }
-
-            List<Medborgare> medborgare = new List<Medborgare>();
-            for (int i = 0; i < 5; i++)
-            {
-                medborgare.Add(new Medborgare(new List<string>()));
-            }
+              
+                Console.CursorVisible = false;
 
 
-            PrintFrame();
-            PrintStatus();
-            PrintPrison();
-            PrintNews();
+                PrintFrame();
 
-            while (true)
-            {
-                PrintPersons(poliser, tjuvar, medborgare);
+                Stad stad = new Stad();
+                stad.PrintStatus();
+                stad.PrintNews();
+                stad.PrintPrison();
+                stad.Start();
             }
         }
-        private static void PrintStatus()
-        {
-            Console.SetCursorPosition(2, 7);
-            Console.WriteLine("''''''''''''''''''''''''''''''TEST''''''''''''''''''''''''''''''");
-            Console.SetCursorPosition(2, 8);
-            Console.WriteLine("Poliser: HarriHaffa, HarriHaffa, HarriHaffa, HarriHaffa");
-            Console.SetCursorPosition(2, 9);
-            Console.WriteLine("Tjuvar: Boven, boven boven boven ");
-            Console.SetCursorPosition(2, 10);
-            Console.WriteLine("Medborgare: fisen fisen fisen fisen");
-        }
-        private static void PrintNews()
-        {
-            Console.SetCursorPosition(2, 17);
-            Console.WriteLine("''''''''''''''''''''''''''''''TEST''''''''''''''''''''''''''''''");
-            Console.SetCursorPosition(2, 18);
-            Console.WriteLine("Polisen grep Alex");
-            Console.SetCursorPosition(2, 19);
-            Console.WriteLine("Tjuvarn skrek högt");
-            Console.SetCursorPosition(2, 20);
-            Console.WriteLine("Medborgaren sprang");
-        }
-        private static void PrintPrison()
-        {
-            for (int i = 7; i < 25; i++)
-            {
-                Console.SetCursorPosition(69, i);
-                Console.WriteLine("''''''''''''TEST'''''''''''''''");
-            }
-        }
-        private static void PrintPersons(List<Polis> poliser, List<Tjuv> tjuvar, List<Medborgare> medborgare)
-        {
-            foreach (var polis in poliser)
-            {
-                polis.Position();
-                polis.RandomDirection();
-                polis.PositionWithSymbol();
-            }
-
-            foreach (var tjyv in tjuvar)
-            {
-                tjyv.Position();
-                tjyv.RandomDirection();
-                tjyv.PositionWithSymbol();
-            }
-
-            foreach (var medborgar in medborgare)
-            {
-                medborgar.Position();
-                medborgar.RandomDirection();
-                medborgar.PositionWithSymbol();
-            }
-            Thread.Sleep(200);
-        }
-
+       
         private static void PrintFrame()
         {
             Console.WriteLine(@"
---
+++ ConsoleApp29/Stad.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace TjuvOchPolis
+{
+    internal class Stad
+    {
+        private List<Polis> poliser = new List<Polis>();
+        private List<Tjuv> tjuvar = new List<Tjuv>();
+        private List<Medborgare> medborgare = new List<Medborgare>();
+
+        public Stad()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                poliser.Add(new Polis(new List<string>()));
+                tjuvar.Add(new Tjuv(new List<string>()));
+                medborgare.Add(new Medborgare(new List<string>()));
+            }
+        }
+
+        public void Start()
+        {
+            while (true)
+            {
+                PrintPersons();
+                Thread.Sleep(200);
+            }
+        }
+
+        private void PrintPersons()
+        {
+            foreach (var polis in poliser)
+            {
+                polis.Position();             
+                polis.RandomDirection();       
+                polis.PositionWithSymbol();    
+            }
+
+            foreach (var tjuv in tjuvar)
+            {
+                tjuv.Position();
+                tjuv.RandomDirection();
+                tjuv.PositionWithSymbol();
+            }
+
+            foreach (var civil in medborgare)
+            {
+                civil.Position();
+                civil.RandomDirection();
+                civil.PositionWithSymbol();
+            }
+        }
+
+        public void PrintStatus()
+        {
+            Console.SetCursorPosition(2, 7);
+            Console.WriteLine("''''''''''''''''''''''''''''''STATUS''''''''''''''''''''''''''''");
+            Console.SetCursorPosition(2, 8);
+            Console.WriteLine("Poliser: HarriHaffa x5");
+            Console.SetCursorPosition(2, 9);
+            Console.WriteLine("Tjuvar: Boven x5");
+            Console.SetCursorPosition(2, 10);
+            Console.WriteLine("Medborgare: Fisen x5");
+        }
+
+        public void PrintNews()
+        {
+            Console.SetCursorPosition(2, 17);
+            Console.WriteLine("''''''''''''''''''''''''''''''NEWS''''''''''''''''''''''''''''''");
+            Console.SetCursorPosition(2, 18);
+            Console.WriteLine("Polisen grep Alex");
+            Console.SetCursorPosition(2, 19);
+            Console.WriteLine("Tjuvarn skrek högt");
+            Console.SetCursorPosition(2, 20);
+            Console.WriteLine("Medborgaren sprang");
+        }
+
+        public void PrintPrison()
+        {
+            for (int i = 7; i < 25; i++)
+            {
+                Console.SetCursorPosition(69, i);
+                Console.WriteLine("''''''''''''TEST'''''''''''''''");
+            }
+        }
+    }
+}
